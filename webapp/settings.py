bf64b0dbdc8bba938926877c144e170a51ae1256
Zejd Imamovic <i.zejd@hotmail.com>
2024-05-31 11:26:38 +0200

    MySQL database involved

--- webapp/settings.py
+++ webapp/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'truckdb',  # Your MySQL database name
+        'USER': 'adminTB',
+        'PASSWORD': 'arvin123',
+        'HOST': '127.0.0.1',  # Use '127.0.0.1' if needed
+        'PORT': '3306',  # Default MySQL port
     }
 }
 
